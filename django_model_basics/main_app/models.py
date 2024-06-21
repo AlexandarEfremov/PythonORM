822b9f4f8f589b66de254a800a1898f927a735a1
Alexander <efremov.alex@yahoo.bg>
2024-06-21 21:23:46 +0300

    21.06.2024

--- django_model_basics/main_app/models.py
+++ django_model_basics/main_app/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 
@@ -35,6 +37,42 @@ class Department(models.Model):
     last_edited_on = models.DateTimeField(
         auto_now=True,
         editable=False)
+
+
+class Project(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True
+    )
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+    budget = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+    duration_in_days = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        verbose_name="Estimated Hours"
+    )
+    start_date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Start Date",
+        default=date.today
+    )
+    created_on = models.DateTimeField(
+        default=date.today,
+        editable=False
+    )
+    last_edited_on = models.DateTimeField(
+        default=date.today,
+        editable=False
+    )
 
 
 
