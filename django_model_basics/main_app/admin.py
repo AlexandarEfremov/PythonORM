2d821cbf7bbcca12133199217976ca62754b7c1e
Alexander <efremov.alex@yahoo.bg>
2024-06-21 22:17:40 +0300

    21.06.2024

--- django_model_basics/main_app/admin.py
+++ django_model_basics/main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Department, Employee, Project
+
+admin.site.register(Department)
+admin.site.register(Employee)
+admin.site.register(Project)
 # Register your models here.
