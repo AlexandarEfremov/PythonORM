8625230216c62b442dbb384337cdd17f65fb584d
Alexander <efremov.alex@yahoo.bg>
2024-06-22 23:24:59 +0300

    22.06.2024

--- django_model_basics_exercise/main_app/models.py
+++ django_model_basics_exercise/main_app/models.py
@@ -77,3 +77,27 @@ class UserProfile(models.Model):
     )
 
 
+class Exercise(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    description = models.TextField()
+    difficulty_level = models.CharField(
+        max_length=20
+    )
+    duration_minutes = models.PositiveIntegerField()
+    equipment = models.CharField(
+        max_length=90
+    )
+    video_url = models.URLField(
+        blank=True,
+        null=True
+    )
+    calories_burned = models.PositiveIntegerField(
+        default=1
+    )
+    is_favorite = models.BooleanField(
+        default=False
+    )
+
+
