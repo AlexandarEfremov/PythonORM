bdf84d1724dc5ed61dedbe65bb6d98280295df6e
Aleksandar Efremov <145782693+AlexandarEfremov@users.noreply.github.com>
2024-06-22 23:59:18 +0300

    Update settings.py

--- django_model_basics_exercise/orm_skeleton/settings.py
+++ django_model_basics_exercise/orm_skeleton/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "django_models_exercise",
         "USER": "postgres",
-        "PASSWORD": "finalsix",
+        "PASSWORD": "*******",
         "HOST": "localhost",
         "PORT": "5432",
     }
