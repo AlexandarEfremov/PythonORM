3a4108884bde3091696a5299a27196550bc5355c
Alexander <efremov.alex@yahoo.bg>
2024-06-24 23:49:51 +0300

    24.06.2024

--- migrations_and_django_admin/main_app/admin.py
+++ migrations_and_django_admin/main_app/admin.py
@@ -1,4 +1,18 @@
-from main_app.models import Product
+from .models import Product
 from django.contrib import admin
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'created_on')
+    search_fields = ('name', 'category', 'supplier')
+    list_filter = ('category', 'supplier')
+    fieldsets = (
+        ('General Information', {
+            'fields': ('name', 'description', 'price', 'barcode')
+        }),
+        ('Categorization', {
+            'fields': ('category', 'supplier')
+        }),
+    )
+    date_hierarchy = 'created_on'
