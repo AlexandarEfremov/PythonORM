ae5ebac76e0e1cd5abd5ab934b22484c21b0c122
Alexander <efremov.alex@yahoo.bg>
2024-06-25 22:49:03 +0300

    25.06.2024

--- migrations_and_django_admin_exercise/main_app/models.py
+++ migrations_and_django_admin_exercise/main_app/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Shoe(models.Model):
     brand = models.CharField(max_length=25)
     size = models.PositiveIntegerField()
+
+#Commands executed:
+#python manage.py makemigrations
+#python manage.py migrate
+
+#result 0001_initial.py
