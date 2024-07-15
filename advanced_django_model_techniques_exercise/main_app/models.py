36deb56ce3ce7e271ae252f40aa058b59d506253
Alexander <efremov.alex@yahoo.bg>
2024-07-15 21:43:54 +0300

    15.07.2024

--- advanced_django_model_techniques_exercise/main_app/models.py
+++ advanced_django_model_techniques_exercise/main_app/models.py
@@ -1,6 +1,6 @@
 
 from django.core.exceptions import ValidationError
-from django.core.validators import RegexValidator, MinValueValidator, EmailValidator, URLValidator
+from django.core.validators import RegexValidator, MinValueValidator, EmailValidator, URLValidator, MinLengthValidator
 from django.db import models
 
 
@@ -36,6 +36,67 @@ class Customer(models.Model):
     )
 
 
+class BaseMedia(models.Model):
+    title = models.CharField(
+        max_length=100,
+    )
+    description = models.TextField()
+    genre = models.CharField(
+        max_length=50,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at', 'title']
+
+
+class Book(BaseMedia):
+    author = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(5, "Author must be at least 5 characters long")
+        ]
+    )
+    isbn = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[
+            MinLengthValidator(6, "ISBN must be at least 6 characters long")
+        ]
+    )
+
+    class Meta(BaseMedia.Meta):
+        verbose_name = "Model Book"
+        verbose_name_plural = "Models of type - Book"
+
+
+class Movie(BaseMedia):
+    director = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(8, "Director must be at least 8 characters long")
+        ]
+    )
+
+    class Meta(BaseMedia.Meta):
+        verbose_name = "Model Movie"
+        verbose_name_plural = "Models of type - Movie"
+
+
+class Music(BaseMedia):
+    artist = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(9, "Artist must be at least 9 characters long")
+        ]
+    )
+
+    class Meta(BaseMedia.Meta):
+        verbose_name = "Model Music"
+        verbose_name_plural = "Models of type - Music"
 
 
 # Create your models here.
