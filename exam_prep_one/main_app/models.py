e9818cd909a67086f4bda2e72790cbef80c56db6
Alexander <efremov.alex@yahoo.bg>
2024-07-18 22:55:50 +0300

    18.07.2024

---
+++ exam_prep_one/main_app/models.py
@@ -0,0 +1,120 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+class Director(models.Model):
+    full_name = models.CharField(
+        max_length=120,
+        validators=[
+            MinLengthValidator(2),
+            MaxLengthValidator(120)
+        ]
+    )
+    birth_date = models.DateField(
+        default='1900-01-01'
+    )
+    nationality = models.CharField(
+        default='Unknown',
+        max_length=50,
+        validators=[
+            MaxLengthValidator(50),
+        ]
+    )
+    years_of_experience = models.SmallIntegerField(
+        default=0,
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+
+
+class GenreChoices(models.TextChoices):
+    ACTION = 'Action', 'Action'
+    COMEDY = 'Comedy', 'Comedy'
+    DRAMA = 'Drama', 'Drama'
+    OTHER = 'Other', 'Other'
+
+
+class Actor(models.Model):
+    full_name = models.CharField(
+        max_length=120,
+        validators=[
+            MinLengthValidator(2),
+            MaxLengthValidator(120)
+        ]
+    )
+    birth_date = models.DateField(
+        default='1900-01-01'
+    )
+    nationality = models.CharField(
+        default='Unknown',
+        max_length=50,
+        validators=[
+            MaxLengthValidator(50)
+        ]
+    )
+    is_awarded = models.BooleanField(
+        default=False
+    )
+    last_updated = models.DateField(
+        auto_now=True,
+    )
+
+
+class Movie(models.Model):
+
+    title = models.CharField(
+        max_length=150,
+        validators=[
+            MinLengthValidator(5),
+            MaxLengthValidator(150)
+        ]
+    )
+    release_date = models.DateField()
+    storyline = models.TextField(
+        blank=True,
+        null=True,
+    )
+    genre = models.CharField(
+        choices=GenreChoices,
+        default='Other',
+        max_length=6,
+        validators=[
+            MaxLengthValidator(6)
+        ]
+    )
+    rating = models.DecimalField(
+        decimal_places=1,
+        default=0.0,
+        max_digits=3,
+        validators=[
+            MinValueValidator(0.0),
+            MaxValueValidator(10.0)
+        ]
+    )
+    is_classic = models.BooleanField(
+        default=False
+    )
+    is_awarded = models.BooleanField(
+        default=False
+    )
+    last_updated = models.DateField(
+        auto_now=True
+    )
+    director = models.ForeignKey(
+        to='Director',
+        on_delete=models.CASCADE,
+    )
+    starring_actor = models.ForeignKey(
+        to='Actor',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='starred_movies'
+    )
+    actors = models.ManyToManyField(
+        to='Actor'
+    )
+
+# Create your models here.
