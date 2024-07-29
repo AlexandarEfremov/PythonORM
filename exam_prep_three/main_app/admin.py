71f166206f5d19095801626861c379d2136ddd4b
Alexander <efremov.alex@yahoo.bg>
2024-07-29 22:10:41 +0300

    29.07.2024

--- exam_prep_three/main_app/admin.py
+++ exam_prep_three/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from exam_prep_three.main_app.models import TennisPlayer
+from exam_prep_three.main_app.models import TennisPlayer, Tournament
 
 
 @admin.register(TennisPlayer)
@@ -8,5 +8,12 @@ class TennisPlayerAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'country', 'ranking', 'is_active']
     list_filter = ['is_active']
     search_fields = ['full_name', 'country']
+
+
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'location', 'prize_money', 'surface_type', 'start_date']
+    list_filter = ['surface_type']
+    search_fields = ['name', 'location']
 
 
