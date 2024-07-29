59f4c6a96bddee882d77493759b486880d7978d0
Alexander <efremov.alex@yahoo.bg>
2024-07-29 22:09:18 +0300

    29.07.2024

--- exam_prep_three/main_app/admin.py
+++ exam_prep_three/main_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from exam_prep_three.main_app.models import TennisPlayer
+
+
+@admin.register(TennisPlayer)
+class TennisPlayerAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'country', 'ranking', 'is_active']
+    list_filter = ['is_active']
+    search_fields = ['full_name', 'country']
+
+
