d7e310df37e62438ad76dda0e392babc2efeadc2
Alexander <efremov.alex@yahoo.bg>
2024-07-30 22:11:17 +0300

    30.07.2024

---
+++ exam_prep_three/caller.py
@@ -0,0 +1,35 @@
+import os
+import django
+from django.db.models import Q
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+from main_app.models import TennisPlayer
+
+
+def get_tennis_players(search_name=None, search_country=None):
+    if search_name is None and search_country is None:
+        return ""
+    main_query = ""
+
+    both_query = Q(full_name__icontains=search_name) & Q(country__icontains=search_country)
+    name_query = Q(full_name__icontains=search_name)
+    country_query = Q(country__icontains=search_country)
+
+    if search_name and search_country:
+        main_query = both_query
+    elif search_name is None and search_country:
+        main_query = country_query
+    elif search_country is None and search_name:
+        main_query = name_query
+
+    player = TennisPlayer.objects.filter(main_query).order_by("ranking")
+
+    if not player:
+        return ""
+
+    return "\n".join(f"Tennis Player: {p.full_name}, country: {p.country}, ranking: {p.ranking}" for p in player)
+
+
